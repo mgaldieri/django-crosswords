ca369cd36067fb99907cac2e36c6e4f2664d7da4
mgaldieri <mgaldieri@gmail.com>
2014-05-18 23:33:30 -0300

    Added english and spanish characters.

--- main/utils/chars.py
+++ main/utils/chars.py
@@ -12,4 +12,28 @@ class _pt_BR():
         self.uppercase = self.upperchars + self.upperaccented
         self.allchars = self.lowercase + self.uppercase
 
+
+class _en_US():
+    def __init__(self):
+        self.lowerchars = list(u'abcdefghijklmnopqrstuvwxyz')
+        self.upperchars = list(u'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        self.loweraccented = list(u'')
+        self.upperaccented = list(u'')
+        self.lowercase = self.lowerchars + self.loweraccented
+        self.uppercase = self.upperchars + self.upperaccented
+        self.allchars = self.lowercase + self.uppercase
+
+
+class _es_ES():
+    def __init__(self):
+        self.lowerchars = list(u'abcdefghijklmnopqrstuvwxyz')
+        self.upperchars = list(u'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        self.loweraccented = list(u'áàâéíóúüñ')
+        self.upperaccented = list(u'ÁÀÂÉÍÓÚÜÑ')
+        self.lowercase = self.lowerchars + self.loweraccented
+        self.uppercase = self.upperchars + self.upperaccented
+        self.allchars = self.lowercase + self.uppercase
+
 pt_BR = _pt_BR()
+en_US = _en_US()
+es_ES = _es_ES()
